5113104670b013388a6aa7c6d9c9966d9fa6f951
Alex <alex@example.com>
2026-09-01 19:01:39 +0200

    Update push script

--- push.bat
+++ push.bat
@@ -1,6 +1,6 @@
 @echo off
 cd /d "%~dp0"
-"C:\Program Files (x86)\Git\cmd\git.exe" add users/
-"C:\Program Files (x86)\Git\cmd\git.exe" commit -m "Update user collection"
-"C:\Program Files (x86)\Git\cmd\git.exe" push
+"C:\Program Files\Git\cmd\git.exe" add users/
+"C:\Program Files\Git\cmd\git.exe" commit -m "Update user collection"
+"C:\Program Files\Git\cmd\git.exe" push
 pause
